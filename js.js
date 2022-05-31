f6f01f25f072f0941e62a4d99d3e961584b4ff7a
JokerRoller <BECOOL686868@gmail.com>
2022-05-31 15:10:13 +0400

    asd

---
+++ js.js
@@ -0,0 +1,10 @@
+var cinema = {
+    name: "CineWorld", 
+    language: ["ENG", "ES", "JP"],
+    adress: "Earth",
+    movies: [
+        {name:"The Shawshank Redemption", IMBD: 9.2},
+        {name: "The Godfather", IMBD: 9.2},
+        {name: "The Dark Knight", IMBD: 9.2}
+    ]
+}
